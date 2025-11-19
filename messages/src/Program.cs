ae680487d45ed90a9ea0d7765a79987d99090006
Frank Pommerening <frank@pommerening-online.de>
2025-11-19 18:07:39 +0000

    add message application

---
+++ messages/src/Program.cs
@@ -0,0 +1,29 @@
+using FP.ContainerTraining.Messages.Data;
+using FP.ContainerTraining.Messages.Components;
+using MudBlazor.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddMudServices();
+builder.Services.AddSingleton<IMessageRepository, MessageRepository>();
+builder.Configuration.AddEnvironmentVariables();
+
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+}
+
+app.MapGet("/time", () => DateTimeOffset.Now);
+
+app.UseAntiforgery();
+app.UseStaticFiles();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
